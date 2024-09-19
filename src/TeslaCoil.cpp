87ed77e831a8e6c8fde617f3e716c411abda06bf
Bas Paap <baspaap@live.nl>
2024-09-20 01:03:52 +0200

    Configure the tesla coil pin as output.

--- src/TeslaCoil.cpp
+++ src/TeslaCoil.cpp
@@ -8,6 +8,7 @@ Bas::Elerion::TeslaCoil::TeslaCoil(int relayPin) : relayPin{relayPin}
 void Bas::Elerion::TeslaCoil::begin()
 {
 	Serial.println("Starting Tesla Coil");
+	pinMode(this->relayPin, OUTPUT);
 }
 
 void Bas::Elerion::TeslaCoil::update()
@@ -17,12 +18,16 @@ void Bas::Elerion::TeslaCoil::update()
 
 void Bas::Elerion::TeslaCoil::turnOn()
 {
+	Serial.print("Turning tesla coil ON on pin ");
+	Serial.println(this->relayPin);
 	digitalWrite(this->relayPin, HIGH);
 	delayedFunction.begin(onDurationInMilliseconds, [this]{ turnOff(); });
 }
 
 void Bas::Elerion::TeslaCoil::turnOff()
 {
+	Serial.print("Turning tesla coil OFF on pin");
+	Serial.println(this->relayPin);
 	digitalWrite(this->relayPin, LOW);
 	delayedFunction.begin(offDurationInMilliseconds, [this]{ turnOn(); });
 }
